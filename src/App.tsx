ce2e39b95f28c46027bef9e1919fed6dc912a1f5
pranta-barua007 <prantabarua247@gmail.com>
2023-03-20 11:44:28 +0600

    dev setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import './App.css'
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <p className="text-bold text-sky-600">
-       fresh start
-      </p>
+      <p className="text-bold text-sky-600">fresh start</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
