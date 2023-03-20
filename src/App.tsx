755897f3d904813cbea7efb366554ff47e8bdd02
pranta-barua007 <prantabarua247@gmail.com>
2023-03-20 11:12:41 +0600

    dev environment setup with vite

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <p className="text-bold text-sky-600">
+       fresh start
+      </p>
+    </div>
+  )
+}
+
+export default App
